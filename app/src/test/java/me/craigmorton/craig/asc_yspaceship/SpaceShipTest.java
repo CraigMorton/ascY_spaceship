fbaf064b89cf29833a19601853abe7761994c1f0
Craig Morton <CraigMorton22@gmail.com>
2016-11-28 16:08:20 +0000

    Add SpaceShipTest file

---
+++ app/src/test/java/me/craigmorton/craig/asc_yspaceship/SpaceShipTest.java
@@ -0,0 +1,18 @@
+package me.craigmorton.craig.asc_yspaceship;
+
+import org.junit.Before;
+
+/**
+ * Created by user on 24/11/2016.
+ */
+
+public class SpaceShipTest {
+
+    CanvasEntity ship;
+
+    @Before
+    public void before() {
+
+    }
+
+}
